0b8c17152eea592a84ef6af32d96c4b67ce5fed6
maly7 <paul.aly@outlook.com>
2019-12-11 20:29:26 -0500

    Initial setup files

---
+++ src/main/java/com/lumberjackdev/java11authapi/Java11authapiApplication.java
@@ -0,0 +1,13 @@
+package com.lumberjackdev.java11authapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java11authapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java11authapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lumberjackdev/java11authapi/Java11authapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lumberjackdev.java11authapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java11authapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
